e9fc1cbe8e0c084cfb3306718a9b3f477ce82c89
DiamondPurvis <purvisdiamond@gmail.com>
2019-01-22 21:46:29 -0500

    Completed implicit binding -atlanta is in georgia

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window binding - its usually the default behavior
+* 2. implicit binding - left of the period when it is invoked
+* 3. explicit binding - lets you use the call or apply function to pass a reference
+* 4. new binding - using the new keyword to call the object that was created
 *
 * write out a code example of each explanation above
 */
@@ -16,6 +16,14 @@
 // Principle 2
 
 // code example for Implicit Binding
+const myObj = {
+    place: 'Atlanta',
+    city: function(state) {
+        console.log(`${this.place} is in ${state}`);
+        console.log(this);
+    }
+};
+myObj.city('Georgia');
 
 // Principle 3
 
